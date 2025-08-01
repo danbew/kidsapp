fd7aaa1e84b870202a3dab777efc3f4ba2b2802f
Dan123655 <106719160+Dan123655@users.noreply.github.com>
2025-08-01 20:56:19 +0200

    feat(home): select button

---
+++ src/components/SelectButton.tsx
@@ -0,0 +1,48 @@
+import { Image, Pressable, StyleSheet, Text, View } from 'react-native';
+type Props = {
+  title?: string;
+  onPress: () => void;
+};
+const SelectButton = ({ title = 'Все темы', onPress }: Props) => {
+  return (
+    <Pressable onPress={onPress} style={styles.button}>
+      <Text style={styles.text}>{title}</Text>
+      <View style={styles.iconContainer}>
+        <Image
+          source={require('../../assets/icons/down-poiner.png')}
+          style={styles.icon}
+        />
+      </View>
+    </Pressable>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.2)',
+    flexDirection: 'row',
+    borderRadius: 24,
+    padding: 5,
+    paddingLeft: 10,
+    gap: 8,
+  },
+  text: {
+    color: 'white',
+    fontFamily: 'Nunito',
+    fontWeight: '800',
+    lineHeight: 12,
+    fontSize: 12,
+  },
+  iconContainer: {
+    backgroundColor: 'rgba(0, 0, 0, 0.2)',
+    borderRadius: 9,
+    width: 18,
+    height: 18,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  icon: {},
+});
+export default SelectButton;
